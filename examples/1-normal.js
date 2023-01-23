f608d8579e17b43fc1309ef8d4f12710fecf7f54
proxnull <proxnull@mail.ru>
2023-01-23 06:28:18 +0200

    add UDP support, add localAddress, PRETTIER FORMAT

--- examples/1-normal.js
+++ examples/1-normal.js
@@ -1,7 +1,17 @@
-'use strict';
+"use strict";
 
-const socks5 = require('..');
+const socks5 = require("..");
+const ipv4 = require("../utils/ipv4");
 
-const server = socks5.createServer();
+const server = socks5.createServer({
+  logger: {
+    // debug: function() {}, /* Disable debug messages */
+    debug: console.debug,
+    info: console.info,
+    warn: console.warn,
+    error: console.error,
+  },
+  // localAddress: "192.168.0.100", /* Local Interface address */
+});
 
 server.listen(1080);
